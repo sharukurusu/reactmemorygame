73057d63ce39ded53f7c2a958f7ddf1a8359e048
M.Zarli <16181950+sharukurusu@users.noreply.github.com>
2019-01-16 00:37:06 -0500

    Updates

---
+++ precache-manifest.34a240d1b5d87043f4ef21b618c4bc6c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "21bdc890cc733f23f905",
+    "url": "/reactmemorygame/static/js/runtime~main.21bdc890.js"
+  },
+  {
+    "revision": "8f4e4047a2f0aedcc4df",
+    "url": "/reactmemorygame/static/js/main.8f4e4047.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/reactmemorygame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "662c9b88965f264a708b9a61d03898c1",
+    "url": "/reactmemorygame/index.html"
+  }
+];
